82d21112f5229b1bc36aadef656e541375c80265
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-05-05 19:21:44 +0200

    FIX: Error Page path

--- First-version/src/Store/Router.js
+++ First-version/src/Store/Router.js
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../Pages/Root';
 import HomePage from '../Pages/Home'
-import ErrorPage from './Error';
+import ErrorPage from '../Pages/Error';
+import ContactPage from '../Pages/Contact'
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage/>,
     children: [
       { path: "/", element: <HomePage /> },
+      { path: "/contacts/:contactId", element: <ContactPage/> },
     ],
   },
 ]);
