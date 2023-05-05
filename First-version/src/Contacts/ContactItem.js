18dfd7beec0d9b01fec863f62e94ad0727c3386e
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-05-05 19:22:51 +0200

    UPDATE: routing on Contact Item

--- First-version/src/Contacts/ContactItem.js
+++ First-version/src/Contacts/ContactItem.js
@@ -1,22 +1,24 @@
 import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import ContactsContext from "../Store/contacts-context.js";
 import classes from "./ContactItem.module.scss";
 
 const ContactItem = (props) => {
   const contCtx = useContext(ContactsContext);
+  const navigate = useNavigate();
 
   const contactViewHandler = () => {
     contCtx.displayItem({ ...props.contact });
-    props.onClick();
+    navigate(`/contacts/${props.contact.id}`);
   };
 
   return (
     props.contact.showing && (
       <li className={`col-lg-4 col-md-6 ${classes.contact}`}>
-        <div className={classes["contact-name"]} onClick={contactViewHandler}>
+        <Link className={classes["contact-name"]} onClick={contactViewHandler}>
           {props.contact.name}
-        </div>
+        </Link>
         <div className={classes["contact-info"]}>
           <p>{props.contact.email}</p>
           <p>{props.contact.phone}</p>
