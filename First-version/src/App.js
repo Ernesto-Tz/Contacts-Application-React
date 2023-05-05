337a8153027dfb26f1944bc648e735e492059a46
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-05-05 18:01:22 +0200

    IMPLEMENT: Router into App.js

--- First-version/src/App.js
+++ First-version/src/App.js
@@ -1,47 +1,8 @@
-import { useState } from "react";
-
-import ContactsProvider from "./Store/ContactsProvider";
-import Header from "./Layout/Header/Header";
-import Footer from "./Layout/Footer/Footer";
-import AddButton from "./UI/AddButton";
-import TitleBar from "./Layout/TitleBar/TitleBar";
-import ContactsList from "./Contacts/ContactsList";
-import ContactView from "./Contacts/ContactView/ContactView";
-import AddContact from "./Contacts/AddContact/AddContact";
+import { RouterProvider } from 'react-router-dom';
+import router from './Store/Router'
 
 function App() {
-  const [showContactView, setShowContactView] = useState(false);
-  const [showAddContact, setShowAddContact] = useState(false);
-  const [contactActions, setContactActions] = useState("");
-
-  const contactViewHandler = () => {
-    setShowContactView(true);
-    setContactActions("VIEW");
-  };
-
-  return (
-    <ContactsProvider>
-      {showContactView && (
-        <ContactView
-          onClose={() => setShowContactView(false)}
-          onEditContact={() => setContactActions("EDIT")}
-          action={contactActions}
-        />
-      )}
-      {showAddContact && (
-        <AddContact onClose={() => setShowAddContact(!showAddContact)} />
-      )}
-      <Header />
-      <main>
-        <TitleBar />
-        <ContactsList contactViewActive={contactViewHandler} />
-        <AddButton onClick={() => setShowAddContact(!showAddContact)}>
-          +
-        </AddButton>
-      </main>
-      <Footer />
-    </ContactsProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
