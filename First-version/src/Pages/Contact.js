8f8b41deb8b9faeb5d341dbc26e5b2e611e0188a
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-05-11 14:09:02 +0200

    UPDATE: Contact Page

--- First-version/src/Pages/Contact.js
+++ First-version/src/Pages/Contact.js
@@ -1,13 +1,8 @@
-import { useParams } from "react-router-dom";
 import ContactView from "../Contacts/ContactView/ContactView";
 
 function ContactDetailsPage() {
-  const params = useParams();
-
   return (
     <>
-      <h1>Contact Details</h1>
-      <p>{params.contactId}</p>
       <ContactView/>
     </>
   );
